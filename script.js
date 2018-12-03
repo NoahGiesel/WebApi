baa08f84c8bb39eea8fc629b8e45cf8f80f1766b
Noah Giesel <44894110+NoahGiesel@users.noreply.github.com>
2018-12-03 15:18:11 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+ var firstname = document.getElementById('name');
+
+ var surname = document.getElementById('surname');
+
+
+function showContent(){
+
+    var userData = [];
+
+
+    userData.push(firstname.value);
+    userData.push(surname.value);
+
+
+    console.log(userData);
+
+
+}
